cc5c71b1e302808d66161ee7d4f86e50eeb7973e
Diego Claudino <claudinodiego98@gmail.com>
2026-09-09 08:35:36 -0300

    Add files via upload

---
+++ src/main/java/projeto/Avancado/projeto/repository/dashboardRepository.java
@@ -0,0 +1,7 @@
+package projeto.Avancado.projeto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import projeto.Avancado.projeto.model.dashboard;
+
+public interface dashboardRepository extends JpaRepository<dashboard, String> {
+}
